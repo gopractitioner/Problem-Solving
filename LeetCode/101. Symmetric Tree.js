d634888caf9764d5c50401437214cedf05b78d5a
Junhui Zhang <junhuizhang.g@gmail.com>
2024-07-24 22:58:32 +1200

    m

---
+++ LeetCode/101. Symmetric Tree.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+function equal(p, q) {
+    if (p == null && q == null) return true;
+    if (p == null || q == null) return false;
+    if (p.val != q.val) return false;
+    return equal(p.left, q.right) && equal(p.right, q.left);
+}
+var isSymmetric = function (root) {
+    return equal(root.left, root.right);
+};
